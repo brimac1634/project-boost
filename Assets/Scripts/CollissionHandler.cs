d975e4a7421f759b9c4405821dd91453248d6d22
Brian MacPherson <brimac1634@gmail.com>
2021-10-05 13:08:13 +0800

    added sound for collision and success

---
+++ Assets/Scripts/CollissionHandler.cs
@@ -0,0 +1,82 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollissionHandler : MonoBehaviour
+{
+    [SerializeField] float levelLoadDelay = 2f;
+    [SerializeField] AudioClip crashSound;
+    [SerializeField] AudioClip successSound;
+
+    private AudioSource _audioSource;
+
+    private bool _isTransitioning = false;
+
+    private void Start() {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnCollisionEnter(Collision other) {
+        if (_isTransitioning) { return; }
+
+        switch(other.gameObject.tag)
+        {
+            case "Friendly":
+                Debug.Log("Friendly");
+                break;
+            case "Finish":
+                Debug.Log("Finish");
+                StartSuccessSequence();
+                break;
+            default:
+                Debug.Log("Explode");
+                StartCrashSequence();
+                break;
+        }
+    }
+
+    private void StartSuccessSequence()
+    {
+        _isTransitioning = true;
+        // TODO: add particle effect
+        GetComponent<Movement>().enabled = false;
+        
+        _audioSource.Stop();
+        _audioSource.PlayOneShot(successSound);
+
+        Invoke("NextLevel", levelLoadDelay);
+    }
+
+    private void StartCrashSequence()
+    {
+        _isTransitioning = true;
+
+        // TODO: add particle effect
+        GetComponent<Movement>().enabled = false;
+
+        _audioSource.Stop();
+        _audioSource.PlayOneShot(crashSound);
+
+        Invoke("ReloadLevel", levelLoadDelay);
+    }
+
+    private void ReloadLevel()
+    {
+        int _currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(_currentSceneIndex);
+    }
+
+    private void NextLevel()
+    {
+        int _currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int _sceneCount = SceneManager.sceneCount;
+        if (_currentSceneIndex < _sceneCount - 1)
+        {
+            SceneManager.LoadScene(_currentSceneIndex + 1);
+        }
+        else
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,12 +6,18 @@ public class Movement : MonoBehaviour
 {
     [SerializeField] float mainThrust = 1000f;
     [SerializeField] float rotationSpeed = 150f;
+    [SerializeField] AudioClip mainEngineSound;
+    
     private Rigidbody _rigidbody;
+    private AudioSource _audioSource;
 
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
         _rigidbody.useGravity = true;
+
+        _audioSource = GetComponent<AudioSource>();
+        _audioSource.playOnAwake = false;
     }
 
     void Update()
@@ -25,6 +31,14 @@ public class Movement : MonoBehaviour
         if (Input.GetKey(KeyCode.Space))
         {
             _rigidbody.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+            if (!_audioSource.isPlaying)
+            {
+                _audioSource.PlayOneShot(mainEngineSound);
+            }
+        }
+        else
+        {
+            _audioSource.Stop();
         }
     }
 
